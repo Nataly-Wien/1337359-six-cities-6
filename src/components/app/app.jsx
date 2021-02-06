cc2f699502e1b30f54ed2d821cf2f900008e9a1a
Nataly <nataly-wien@yandex.ru>
2021-02-07 01:13:18 +0300

    Добавляет React Router

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,12 +1,35 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import MainPage from '../main-page/main-page';
+import Login from '../login/login';
+import Favorites from '../favorites/favorites';
+import Room from '../room/room';
+import NotFound from '../not-found/not-found';
 
 const App = (props) => {
   const {citiesCount} = props;
 
   return (
-    <MainPage citiesCount={citiesCount} />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <MainPage citiesCount={citiesCount} />
+        </Route>
+        <Route exact path="/login">
+          <Login />
+        </Route>
+        <Route exact path="/favorites">
+          <Favorites />
+        </Route>
+        <Route exact path="/offer/:id">
+          <Room />
+        </Route>
+        <Route>
+          <NotFound />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 };
 
